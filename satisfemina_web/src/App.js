a2dc9cb7d39147590be5fb0393676b036ca1223b
jbenatem <jbenatem@gmail.com>
2020-09-21 00:53:33 -0500

    Scroll automatico en las diferentes secciones

--- satisfemina_web/src/App.js
+++ satisfemina_web/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Modal from './components/Modal';
+import Catalogo from './components/Catalogo';
+import TipoProductos from './components/TipoProducto';
 
 class App extends Component {
   state = {
     mostrar_modal: false
   };
+
+  scroll = (control) => {
+    var elemento = document.getElementById(control);
+    //console.log(elemento)
+    elemento.scrollIntoView('smooth','start');
+  }
     
   showModal = () => {
     this.setState({mostrar_modal:true});
@@ -21,10 +29,24 @@ class App extends Component {
     return (
       <div className="app container">
         <Navbar
-          showModal={this.showModal}         
+          showModal={this.showModal}
+          scroll={this.scroll}        
         />
         <div className="jumbotron">
           <h1 className="display-3">Hello, world!</h1>
+        </div>
+        <div className="jumbotron" id="catalogo">
+          <h2>Catálogo</h2>
+          <p class="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+          <Catalogo/> 
+        </div>
+        <div className="jumbotron" id="quienessomos">
+          <h2>¿Quiénes somos?</h2>
+          <p class="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+        </div>
+        <div className="jumbotron" id="contactenos">
+          <h2>Contáctenos</h2>
+          <p class="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
         </div>
         <Modal
           mostrar_modal={this.state.mostrar_modal}
--- satisfemina_web/src/components/Navbar.js
+++ satisfemina_web/src/components/Navbar.js
@@ -14,10 +14,13 @@ class Navbar extends Component{
                         <a className="nav-link" href="#">Inicio<span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Catálogo</a>
+                        <a onClick={this.props.scroll.bind(this, 'catalogo')} className="nav-link">Catálogo</a>
                     </li>
                     <li className="nav-item">
-                        <a onClick={this.props.showModal} className="nav-link">Nosotros</a>
+                        <a onClick={this.props.scroll.bind(this, 'quienessomos')} className="nav-link">¿Quiénes somos?</a>
+                    </li>
+                    <li className="nav-item">
+                        <a onClick={this.props.scroll.bind(this, 'contactenos')} className="nav-link">Contáctenos</a>
                     </li>
                     </ul>
                 </div>
