7c38a1f934ad169d609f1b853deb695dcb79f3bb
jbenatem <jbenatem@gmail.com>
2020-09-22 03:59:39 -0500

    Sección de contactenos actualizada

--- satisfemina_web/src/App.js
+++ satisfemina_web/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Modal from './components/Modal';
 import Catalogo from './components/Catalogo';
-import TipoProductos from './components/TipoProducto';
+import CarritoCompras from './components/CarritoCompras'
+import logo from './components/gif.gif';
 
 class App extends Component {
   state = {
@@ -33,20 +34,35 @@ class App extends Component {
           scroll={this.scroll}        
         />
         <div className="jumbotron">
-          <h1 className="display-3">Hello, world!</h1>
+          <img src={logo} class="img-fluid" alt="Satis Femina" width="100%" height="auto"/>
         </div>
         <div className="jumbotron" id="catalogo">
           <h2>Catálogo</h2>
-          <p class="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+          <p className="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
           <Catalogo/> 
+        </div>
+        <div className="jumbotron" id="carritocompras">
+          <h2>Carrito de compras</h2>
+          <p className="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+          <CarritoCompras/>
         </div>
         <div className="jumbotron" id="quienessomos">
           <h2>¿Quiénes somos?</h2>
-          <p class="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+          <p className="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
         </div>
         <div className="jumbotron" id="contactenos">
           <h2>Contáctenos</h2>
-          <p class="text-secondary">Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
+          <p className="text-secondary">Sígannos en nuestras redes sociales!</p>
+          <div className="row">
+            <div className="col-md-6">
+              <i class="fab fa-instagram fa-2x"></i>
+              <a class="lead" href="#">  satis.femina.11</a>             
+            </div>  
+            <div className="col-md-6">
+              <i class="fab fa-facebook fa-2x"></i>
+              <a class="lead" href="https://www.facebook.com/Satis-Femina-102655381539922/">  Satis Femina</a>
+            </div>  
+          </div>
         </div>
         <Modal
           mostrar_modal={this.state.mostrar_modal}
